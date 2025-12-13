6b3480696ead79ef58f0153ad82adfcef9eea693
MhmdAlii1 <mhmdalikn4@gmail.com>
2025-12-13 03:23:28 +0200

    End of section 10

--- client/src/app/layouts/Navbar.tsx
+++ client/src/app/layouts/Navbar.tsx
@@ -42,6 +42,7 @@ export default function Navbar() {
               <MenuItemLink to="/activities">Activities</MenuItemLink>
               <MenuItemLink to="/createActivity">Create Activity</MenuItemLink>
               <MenuItemLink to="/counter">Counter</MenuItemLink>
+              <MenuItemLink to="/errors">Errors</MenuItemLink>
             </Box>
             <MenuItem>User Menu</MenuItem>
           </Toolbar>
@@ -50,7 +51,7 @@ export default function Navbar() {
           {() =>
             uiStore.isLoading ? (
               <LinearProgress
-              color="secondary"
+                color="secondary"
                 sx={{
                   position: "absolute",
                   bottom: 0,
